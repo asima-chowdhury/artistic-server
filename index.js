d3d581979485be809e8620c52611f11d6e33499a
asima-chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2021-03-07 15:01:28 +0600

    customer and designer part done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs-extra');
+const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
@@ -8,6 +10,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('addpost'))
+app.use(fileUpload());
 
 const port = 5000;
 
@@ -20,16 +24,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const addPostCollection = client.db("artisticDatabase").collection("customerAddPost");
     const addUserCollection = client.db("artisticDatabase").collection("allUsers");
-
-    app.post('/clientAddPost', (req, res) => {
-        const addPost = req.body;
-        console.log(addPost)
-
-        addPostCollection.insertOne(addPost)
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-    })
+    const designerProfileCollection = client.db("artisticDatabase").collection("designerProfile");
+    const feedbackCollection = client.db("artisticDatabase").collection("feedback");
 
     app.post('/addAllUsers', (req, res) => {
         const addUser = req.body;
@@ -47,5 +43,86 @@ client.connect(err => {
                 return res.send(documents);
             })
     })
+
+    app.post('/designerProfile', (req, res) => {
+        const designer = req.body;
+        console.log(designer)
+
+        designerProfileCollection.insertOne(designer)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+    //----------- Customer feedback start ---------
+    app.post('/addReview', (req, res) => {
+        const name = req.body.name;
+        const projectName = req.body.projectName;
+        const projectReview = req.body.projectReview;
+        // console.log(name,projectName,projectReview);
+
+        feedbackCollection.insertOne({ name, projectName, projectReview })
+            .then(result => {
+                console.log(result);
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.get('/feedback', (req, res) => {
+        feedbackCollection.find({})
+            .toArray((err, documents) => {
+                return res.send(documents);
+            })
+    })
+    //------------ Customer feedback end  ----------
+
+    // app.post('/customerAddPost', (req, res) => {
+    //     const addPost = req.body;
+    //     console.log(addPost)
+
+    //     addPostCollection.insertOne(addPost)
+    //         .then(result => {
+    //             res.send(result.insertedCount > 0);
+    //         })
+    // })
+    //-----------  customer add post start ---------
+    app.post('/customerAddPost', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const email = req.body.email;
+        const project = req.body.project;
+        const details = req.body.details;
+        const price = req.body.price;
+
+        // const filePath = `${__dirname}/addpost/${file.name}`;
+
+        console.log( name, email, project, details, price, image);
+
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimType,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        addPostCollection.insertOne({ name, email, project, details, price, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+    //-----------  customer add post end ---------
+
+    //-----------  customer all project post start ---------
+
+    app.get('/customerProjectList', (req, res) => {
+        addPostCollection.find({})
+            .toArray((err, documents) => {
+                return res.send(documents);
+            })
+    })
+
+    //-----------  customer all project post end ---------
+
 });
 app.listen(process.env.PORT || port);
